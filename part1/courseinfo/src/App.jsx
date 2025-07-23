3e71109e16a7bbd712f33d656a21fc90b28e205e
rightbear <jjj17041113@gmail.com>
2025-07-24 00:30:24 +0800

    docs: finish exercise 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,21 +5,21 @@ const Header = (props) => {
 }
 
 // Use destructuring to props
-const Part = ({ part, exercise }) => {
+const Part = ({part: {name, exercises}}) => {
   return (
     <p>
-        {part} {exercise}
+        {name} {exercises}
     </p>
   )
 }
 
 // Use destructuring to props
-const Content = ({ parts, exercises }) => {
+const Content = ({parts: [part1, part2, part3]}) => {
   return (
     <div>
-      <Part part={parts[0]} exercise={exercises[0]} />
-      <Part part={parts[1]} exercise={exercises[1]} />
-      <Part part={parts[2]} exercise={exercises[2]} />
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
     </div>
   )
 }
@@ -34,21 +34,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content 
         parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
       />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
